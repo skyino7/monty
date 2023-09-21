4884fd918e52c2708f0d5d319ec264e840200412
Skyino7 <christambassador12@gmail.com>
2023-09-21 20:38:56 +0000

    updated

--- monty.h
+++ monty.h
@@ -26,6 +26,7 @@ int n;
 struct stack_s *prev;
 struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -39,6 +40,7 @@ typedef struct instruction_s
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 /**
  * struct plane_t - plane
  *
