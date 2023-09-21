87b68d890f2703ac54c0b4360dae31701a77a3cd
Skyino7 <christambassador12@gmail.com>
2023-09-21 16:42:11 +0000

    corrections done to _div

--- _mul.c
+++ _mul.c
@@ -17,6 +17,16 @@ if (*stack)
 exit(EXIT_FAILURE);
 }
 
+if ((*stack)->n == 0)
+{
+fprintf(stderr, "L%d: division by zero\n", line_number);
+free(plane.line);
+fclose(plane.File);
+if (*stack)
+free_dlistint(*stack);
+exit(EXIT_FAILURE);
+}
+
 (*stack)->next->n *= (*stack)->n;
 *stack = (*stack)->next;
 free((*stack)->prev);
