2c476dfb75e14879478a0baef35dcfa9ab500877
Skyino7 <christambassador12@gmail.com>
2023-09-21 20:33:29 +0000

    updated

--- _mul.c
+++ _mul.c
@@ -1,35 +1,36 @@
 #include "monty.h"
+
 /**
  * _mul - multiplies the top two elements of the stack.
  * @stack: the stack
  * @line_number: line number in the file
  */
+
 void _mul(stack_t **stack, unsigned int line_number)
 {
+	if (!(*stack) || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free(plane.line);
+		fclose(plane.File);
+		if (*stack)
+			free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
 
-if (!(*stack) || !((*stack)->next))
-{
-fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-free(plane.line);
-fclose(plane.File);
-if (*stack)
-	free_dlistint(*stack);
-exit(EXIT_FAILURE);
-}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free(plane.line);
+		fclose(plane.File);
+		if (*stack)
+			free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
 
-if ((*stack)->n == 0)
-{
-fprintf(stderr, "L%d: division by zero\n", line_number);
-free(plane.line);
-fclose(plane.File);
-if (*stack)
-free_dlistint(*stack);
-exit(EXIT_FAILURE);
-}
-
-(*stack)->next->n *= (*stack)->n;
-*stack = (*stack)->next;
-free((*stack)->prev);
-(*stack)->prev = NULL;
+	(*stack)->next->n *= (*stack)->n;
+	*stack = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 
 }
